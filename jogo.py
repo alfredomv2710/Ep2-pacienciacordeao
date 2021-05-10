49e5d537657839b1f3ddfa653f234c390e399e5f
Alfredo <alfredomv@al.insper.edu.br>
2021-05-10 19:17:32 -0300

    escolhe carta

--- jogo.py
+++ jogo.py
@@ -14,7 +14,24 @@ while continua != True and terminar == False:
 while continua:
     baralho = cria_baralho()
     while possui_movimentos_possiveis(baralho):
-        #
+        print("O estado atual do baralho é:\n") 
+        for i in range(len(baralho)):
+           print(str(i + 1) + ". " + baralho[i])
+        numero_da_carta = int(input(f"Escolha uma carta: (1-{len(baralho)})"))
+        numero_da_carta = numero_da_carta - 1
+        movimentos = lista_movimentos_possiveis(baralho, numero_da_carta)
+        while movimentos == []:
+            numero_da_carta = int(input(f"A carta {baralho[numero_da_carta]} não pode ser movida. Escolha uma carta: (1-{len(baralho)})"))
+            numero_da_carta = numero_da_carta -1
+            movimentos = lista_movimentos_possiveis(baralho, numero_da_carta)
+        if movimentos == [1]:
+            
+
+
+
+        break
+
+    continua = False
     if len(baralho) == 1:
         print ('voce ganhou')
     else:
