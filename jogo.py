5948b4d429b730969b60c2699cccf14ce5aed192
pedro <pedrohcperetto@gmail.com>
2021-05-10 18:29:48 -0300

    atualização do jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+continua = False
+terminar = False
+while continua != True:
+    resposta = input('Você quer jogar o jogo? (Digite sim ou nao)\n')
+    if resposta == 'sim':
+        continua = True
+    elif resposta == 'nao':
+
+        print("até breve")
+
