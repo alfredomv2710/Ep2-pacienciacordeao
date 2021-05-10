2f43143468c5b0789a9811b8f4c202a04fe7d2b5
pedro <pedrohcperetto@gmail.com>
2021-05-10 18:44:41 -0300

    jogo2.py

--- jogo.py
+++ jogo.py
@@ -1,10 +1,28 @@
+from funcoes import *
+
+
 continua = False
 terminar = False
-while continua != True:
+while continua != True and terminar == False:
     resposta = input('Você quer jogar o jogo? (Digite sim ou nao)\n')
     if resposta == 'sim':
         continua = True
     elif resposta == 'nao':
-
+        terminar = True
         print("até breve")
+
+while continua:
+    baralho = cria_baralho()
+    while possui_movimentos_possiveis(baralho):
+        #
+    if len(baralho) == 1:
+        print ('voce ganhou')
+    else:
+        print ('voce perdeu')
+    x = input('deseja jogar novamente? (Digite sim ou nao)\n')
+    if x == 'nao':
+        continua = False
+        print('até breve')
+    
+
 
